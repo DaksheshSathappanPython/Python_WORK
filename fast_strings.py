87eaebae7056af4486b6bc892e5d209513a5c3a7
DaksheshSathappanPython <104218113+DaksheshSathappanPython@users.noreply.github.com>
2022-05-12 19:57:30 +0530

    Add files via upload

---
+++ fast_strings.py
@@ -0,0 +1,15 @@
+y=2022
+s='TATA'
+f='05-05-2022'
+
+# print('IPL is sponsored by',s,'from the year',y,\
+# 'and the month is',f)
+
+# print('IPL is sponsored by '+s+' from the year '+str(y)+\
+# ' and the month is '+f)
+
+# print(f'IPL is sponsored by {s} from the year {y} \
+# and the month is {f}')
+
+
+
