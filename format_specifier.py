bff47843219b09a591ae41d178b1d28dc9c3dfc3
DaksheshSathappanPython <104218113+DaksheshSathappanPython@users.noreply.github.com>
2022-05-12 19:58:15 +0530

    Add files via upload

---
+++ format_specifier.py
@@ -0,0 +1,21 @@
+a=5 ; b=3.4 ; c="python"
+# print(a)
+# print(f'{a}')
+# print('%d' %a)
+# print('%i'%a)
+
+b=3.4
+# print(b)
+# print(f'{b}')
+# print('%f'%b)
+# print('%.1f'%b)
+# print('%.2f'%b)
+# print('%.3f'%b)
+# print('%.4f'%b)
+# print('%.5f'%b)
+# print('%12f'%b)
+
+# c='python3'
+# print(c)
+# print(f'{c}')
+# print('%s'%c)
