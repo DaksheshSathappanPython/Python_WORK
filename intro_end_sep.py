32cb0a722cdc12c1007a80a020ed6be73e3a684a
DaksheshSathappanPython <104218113+DaksheshSathappanPython@users.noreply.github.com>
2022-05-12 19:55:51 +0530

    Add files via upload

---
+++ intro_end_sep.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+# print('h','y',sep='@')
+# print(1,2,3,4,5,6,sep='-')
+
+# print('h' 'y')
+# print('h'        'y')
+# print('h'     'y')
+# print('h '     'y')
+# print('h'     ' y')
+# print('h','y')
+# print('h'+'   '+'y')
+print("python")
+print
+print
+
+print('hello')
+print('bye')
+
+
+print('hello',end=',')
+print('bye')
+
+print('hello',end=' ')
+print('bye')
+
+
+
+
+#print(1,end=' ')
+#print(2,end=' ')
+#print(3)
+
+#print('c''b')
+#print('c''b',sep=' ')
+
+#print(1,2,3,4,5,end='\n')
+#print(1,2,3,4,5,end='.')
