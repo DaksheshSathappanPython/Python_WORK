8ef9065706f3927e0ef5766615c917f63816af30
DaksheshSathappanPython <104218113+DaksheshSathappanPython@users.noreply.github.com>
2022-05-12 19:56:16 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,14 @@
+# print('we are learning python programming at evening time \
+# which is at 7pm and today is wednesday , topic \
+# for today is escape sequences.')
+
+# print(len('Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronu\
+# kupokaiwhenuakitanatahu'))
+
+# print('''Taumatawhakatangihangakoauauotamat\
+# eaturipukakapikimaungahoronukupokaiwhenuakitan''')
+
+
+print("pyth'on")
+print('pyth\'on')
+
