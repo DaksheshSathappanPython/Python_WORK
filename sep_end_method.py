eda184c893f66ca3b6c75560eee6c118ae8ea083
DaksheshSathappanPython <104218113+DaksheshSathappanPython@users.noreply.github.com>
2022-05-12 19:55:22 +0530

    Add files via upload

---
+++ sep_end_method.py
@@ -0,0 +1,36 @@
+# print("Iphone" 13)
+# print('h','e','l','l','o')
+# print('h'+'@'+'e'+'@'+'l'+'@'+'l'+'@'+'o')
+# print('h','e','l','l','o',sep='@')
+# print('p' 'o',sep=' ')
+
+
+
+
+# print()
+
+# print('h',end="-")
+# print('i')
+
+
+
+
+
+# print('b',end='')
+# print('y',end='')
+# print('e')
+
+# print('hello '* 5)
+# print('hello\n' * 5)
+
+# print(1,2,3,4,5,sep='\n')
+
+# print(1,2,3,4,5,end='$ ')
+
+#print(1,2,3,4,5,sep='$ ',end='$ ')
+
+# print('Python','3.10.4')
+
+# print('p'+'-'+'version'+'3.10.4') 
+# print('p','3.10.4',sep='-',end=' version') 
+
